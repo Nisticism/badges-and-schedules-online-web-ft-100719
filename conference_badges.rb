380b6280220e6b238a392115c4bfb6305d20e791
Hans Foster <fosterhans@gmail.com>
2019-10-24 04:47:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,4 +4,15 @@ def badge_maker(name)
   puts "Hello, my name is #{name}."
 end
 
-def batch_badge_creator()
+def batch_badge_creator(name_list)
+  messages_array = []
+  name_list.each do |names|
+    messages_array.push(badge_maker(names))
+  end
+  messages_array
+end
+
+def assign_rooms(name_list)
+  assignment_list = []
+  7.times do |num|
+    assignment_list.push("Hello, #{name_list[num]}! You'll be assigned to room #{num + 1}")
